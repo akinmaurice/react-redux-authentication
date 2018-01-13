42840ce754b67236abb29e6d0c51435deea5c11f
akinmaurice <akinmaurice@gmail.com>
2018-01-13 14:10:24 +0100

    styled Auth components

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => (
   <div>
     <div className="container text-center">
       <div className="row">
         <div className="col-lg-12">
-          Login COmponent
+          <h3>
+            <Link to="/" className="headTitle">
+             GramClone
+            </Link>
+          </h3>
+          <h6>
+            Sign In
+          </h6>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form className="form-signin">
+            <div className="form-group">
+              <input type="email" name="email" placeholder="Email Address" className="form-control" />
+            </div>
+            <div className="form-group">
+              <input type="password" name="password" placeholder="Password" className="form-control" />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary btn-block">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => (
   <div>
     <div className="container text-center">
       <div className="row">
         <div className="col-lg-12">
-          Register Component
+          <h3>
+            <Link to="/" className="headTitle">
+             GramClone
+            </Link>
+          </h3>
+          <h6>
+            Create Account
+          </h6>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form className="form-signin">
+            <div className="form-group">
+              <input type="text" name="username" placeholder="Username" className="form-control" />
+            </div>
+            <div className="form-group">
+              <input type="email" name="email" placeholder="Email Address" className="form-control" />
+            </div>
+            <div className="form-group">
+              <input type="password" name="password" placeholder="Password" className="form-control" />
+            </div>
+            <div className="form-group">
+              <input type="password" name="password-confirm" placeholder="Confirm Password" className="form-control" />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary btn-block">
+                Register
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
