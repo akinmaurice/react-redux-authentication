d9452f12995fcc66da4aa3834d669c0007372652
akinmaurice <akinmaurice@gmail.com>
2018-01-13 10:15:19 +0100

    Styled Post Component

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostCard from './utils/PostCard';
 
+
 const PhotoGrid = (props) => {
   const { posts } = props;
   const postList = Object.keys(posts).map(post => <PostCard key={post} details={posts[post]} />);
@@ -8,8 +9,18 @@ const PhotoGrid = (props) => {
     <div className="container text-center">
       <div className="row">
         <div className="col-lg-12">
+          <br />
+          <h3 className="headTitle">
+             GramClone
+          </h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-3 col-md-2 col-sm-1" />
+        <div className="col-lg-6 col-md-8 col-sm-10">
           {postList}
         </div>
+        <div className="col-lg-3 col-md-2 col-sm-1" />
       </div>
     </div>
   );
--- src/components/utils/PostCard.js
+++ src/components/utils/PostCard.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+const bg = require('../../css/bg.jpg');
+
 const PostCard = (props) => {
   const { details } = props;
   return (
-    <p>
-      {details.real_url}
-    </p>
+    <div className="card-body">
+      <div className="postHeader text-left">
+        <div className="round round-lg text-center">
+          <span className="fa fa-user" />
+        </div>
+        &nbsp; UserName
+      </div>
+      <div className="image-wrap">
+        <br />
+        <img src={bg} alt="bgImage" />
+      </div>
+      <div className="postFooter text-left">
+        <br />
+        <ul className="list-inline">
+          <li className="list-inline-item">
+            <i className="fa fa-3x fa-heart-o" />
+          </li>
+        </ul>
+        <p>100 Likes </p>
+      </div>
+    </div>
   );
 };
 
